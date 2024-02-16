3ff3f84052d34ee12d03ccaa41306fc3746a82f5
CalebRdgz <calebrodgz@gmail.com>
2024-02-16 02:58:14 -0600

    ran the application using Jar file I created using Maven

--- quickstart/src/main/java/com/caleb/quickstart/HelloWorldController.java
+++ quickstart/src/main/java/com/caleb/quickstart/HelloWorldController.java
@@ -1,4 +1,14 @@
 package com.caleb.quickstart;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+    //@GetMapping lets Spring know that this is an endpoint at path /hello
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Hello Caleb!";
+    }
+
 }
